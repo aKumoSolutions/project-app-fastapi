1936d7afaa41f2bf5634eb36d1a69b3877e3f72a
Meerim Nurbekova <meerimnurbekova@Meerims-MacBook-Pro.local>
2025-06-18 20:43:33 -0500

    adding code for disk-usage

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ def hello():
 def create_user(user: UserData):
     print(user.name, user.age)
     return {"message": f"Hello, {user.name}!"}
+
+# check_disk_usage(path) Meerim
+
+import shutil
+import os
+
+@app.post("/disk-usage/")
+
+def check_disk_usage(data: PathRequest):
+    path = data.path
+    if not os.path.exists(path):
+        raise HTTPException(status_code=404, detail="Path does not exist")
+    total, used, free = shutil.disk_usage(path)
+    return {
+        "path": path,
+        "total_GB": round(total / (2**30), 2),
+        "used_GB": round(used / (2**30), 2),
+        "free_GB": round(free / (2**30), 2)
+        }
