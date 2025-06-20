4b75e816ad00706951283df935703737a8cfcf1a
abdul.sharif <abdugofir00@gmail.com>
2025-06-20 12:15:24 -0500

    feat: 3 endpoints - get all tasks - get specific taks - update task

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 import os
+from typing import Optional
+import json
 
 app = FastAPI()
 
@@ -11,6 +13,16 @@ class UserData(BaseModel):
 
 class ListDirInput(BaseModel):
     input: str
+
+class TaskData(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[str] = None
+    assignee: Optional[str] = None
+
+with open('tasks.json', 'r') as f:
+    tasks = json.load(f)
+f.close()
 
 @app.get("/hello")
 def hello():
@@ -32,7 +44,6 @@ def list_dir(path: ListDirInput):
             "Error": "There is some error, try again"
         }
 
-
 # Jonathan's Task
 @app.get("/get_public_ip")
 def get_ip():
@@ -43,3 +54,34 @@ def get_ip():
     except:
         return {"message": "Error fetching public IP"}
         
+## abdul's task
+@app.get("/tasks")
+def list_tasks_data():
+    try:
+        return [TaskData(**tdata) for tdata in tasks.items()]
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: str):
+    try:
+        task = tasks.get(task_id)
+        if not task:
+            return {"error": f"Task with id {task_id} not found"}
+        return TaskData(**task)
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: str, task: TaskData):
+    try:
+        if task_id not in tasks:
+            return {"error": f"Task with id {task_id} not found"}
+        for field, value in task.model_dump().items():
+            if value is not None and field != "id":
+                tasks[task_id][field] = value
+        with open('tasks.json', 'w') as f:
+            f.write(json.dumps(tasks))
+        return {"message": f"Task with id {task_id} updated successfully", "task": tasks[task_id]}
+    except Exception as e:
+        return {"error": str(e)}
