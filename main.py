372f6ffcfb5b878abda12ab5b772aeedc26c9ec2
Meerim <marynurbekova@gmail.com>
2025-06-20 10:50:37 -0500

    meerim's task

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
 import os
+import shutil
 
 app = FastAPI()
 
@@ -43,3 +44,17 @@ def get_ip():
     except:
         return {"message": "Error fetching public IP"}
         
+# Meerim's task  check_disk_usage(path)
+
+@app.get("/disk-usage/")
+def check_disk_usage(path: str):
+    try:
+        total, used, free = shutil.disk_usage(path)
+        return {
+            "path": path,
+            "total_GB": round(total / (2**30), 2),
+            "used_GB": round(used / (2**30), 2),
+            "free_GB": round(free / (2**30), 2)
+        }
+    except FileNotFoundError:
+        return {"error": "Path does not exist"}
