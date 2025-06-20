3b9b590e9e586ca1af6cdf7696e314b7ca102ab7
somon <hasomon95@gmail.com>
2025-06-20 15:15:27 -0500

    adding fixed ping function

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ def create_user(user: UserData):
     print(user.name, user.age)
     return {"message": f"Hello, {user.name}!"}
 
-@app.post("/ping_host")
- # Pings a host to check for reachability. Returns True if reachable, False otherwise.
+
+ # Somon's task
+@app.get('/ping/{hostname}')
 def ping_host(hostname: str) -> bool:
     try:
         # -n for Windows, -c for Linux/macOS.
@@ -32,8 +33,11 @@ def ping_host(hostname: str) -> bool:
         command = ['ping', param, '1', hostname]
         result = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         result.returncode == 0:
+        if result.returncode == 0:
+            return True
+        else:
+            return False
     except Exception as e:
-        print(f"An error occurred in ping_host for '{hostname}': {e}")
         return False
 
 # Jonathan's Task
